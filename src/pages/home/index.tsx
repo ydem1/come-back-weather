4136773eab2c6489a83623451722fc878d46160b
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-08-12 17:59:27 +0300

    add material ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,15 @@
 import React, { FC } from "react";
+import { MediaCard } from "src/components/MediaCard";
 
-const Home: FC = () => <h1 className="text-3xl font-bold underline">Home</h1>;
+const Home: FC = () => (
+  <main>
+    <section className="container">
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+    </section>
+  </main>
+);
 
 export default Home;

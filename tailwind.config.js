89b0b65d51bbf8d5bdc4c905dc2fcf5267f58c9f
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-08-12 17:12:51 +0300

    add container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        25: "100px",
+      },
+      maxWidth: {
+        360: "1440px",
+      },
+    },
   },
   plugins: [],
 };

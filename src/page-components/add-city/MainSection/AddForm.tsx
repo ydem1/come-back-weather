657edf2d10a243904f5958a084a420c3a02fbd49
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-08-13 14:13:08 +0300

    add handle sbmit

--- src/page-components/add-city/MainSection/AddForm.tsx
+++ src/page-components/add-city/MainSection/AddForm.tsx
@@ -1,22 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Button, TextField } from "@mui/material";
 
-export const AddForm: FC = () => (
-  <div className="flex-1">
-    <h2>Add City</h2>
+export const AddForm: FC = () => {
+  const [cityQuery, setCityQuery] = useState("");
 
-    <form className="my-5">
-      <TextField
-        className="w-full"
-        id="standard-basic"
-        label="Standard"
-        variant="standard"
-      />
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(cityQuery);
+  };
 
-      <div className="flex gap-5 mt-5">
-        <Button variant="contained">Submit</Button>
-        <Button variant="outlined">Cancel</Button>
-      </div>
-    </form>
-  </div>
-);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCityQuery(event.target.value);
+  };
+
+  return (
+    <div className="flex-1">
+      <h2>Add City</h2>
+
+      <form className="my-5" onSubmit={handleSubmit}>
+        <TextField
+          className="w-full"
+          id="city-query"
+          label="Add new city"
+          variant="standard"
+          value={cityQuery}
+          onChange={handleInputChange}
+        />
+
+        <div className="flex gap-5 mt-5">
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+          <Button type="reset" variant="outlined">
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};

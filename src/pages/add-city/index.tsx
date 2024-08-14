989854b281e8cc5f4ad4e8eed4e267e89a4913d6
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-08-14 17:08:24 +0300

    delete popular list

--- src/pages/add-city/index.tsx
+++ src/pages/add-city/index.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from "react";
 import { PageWrapper } from "src/components/Layouts/PageWrapper";
 import { MainSection } from "src/page-components/add-city/MainSection";
-import { PopularCities } from "src/page-components/add-city/PopularCities";
 
 const AddCity: FC = () => (
-  <PageWrapper mainClassName="flex flex-col gap-10 mt-10">
+  <PageWrapper mainClassName="flex flex-col gap-10 my-10">
     <MainSection />
-    <PopularCities />
   </PageWrapper>
 );
 
